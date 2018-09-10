dfb306d8098b5fd2fd851380b9ed62648cbea1bd
Bruno Meilick <b@bnomei.com>
2018-09-10 15:19:51 +0200

    fixed rss feed
    Signed-off-by: Bruno Meilick <b@bnomei.com>

--- snippets/htmlhead/feed.php
+++ snippets/htmlhead/feed.php
@@ -2,17 +2,19 @@
 	$htmlhead_feed = option('bnomei.htmlhead.feed');
 	if($htmlhead_feed && is_string($htmlhead_feed) && \Kirby\Toolkit\Str::length(trim($htmlhead_feed)) > 0) {
 		if($htmlhead_feedpage = page($htmlhead_feed)) {
-			echo brick('link')
-				->attr('rel',  'alternate')
-				->attr('type', 'application/rss+xml')
-				->attr('href',  $htmlhead_feedpage->url())
-				->attr('title', $htmlhead_feedpage->title()).PHP_EOL;
+			echo \Kirby\Toolkit\HTML::tag('link', '', [
+				'rel' =>  'alternate',
+				'type' => 'application/rss+xml',
+				'href' =>  $htmlhead_feedpage->url(),
+                'title' => $htmlhead_feedpage->title(),
+            ]).PHP_EOL;
 		} else {
-			echo brick('link')
-				->attr('rel',  'alternate')
-				->attr('type', 'application/rss+xml')
-				->attr('href',  url($htmlhead_feed))
-				->attr('title', $htmlhead_feed).PHP_EOL;
+			echo \Kirby\Toolkit\HTML::tag('link', '', [
+				'rel' =>  'alternate',
+				'type' => 'application/rss+xml',
+				'href' =>  url($htmlhead_feed),
+                'title' => $htmlhead_feed,
+            ]).PHP_EOL;
 		}
 	}
 ?>
