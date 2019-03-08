f18d3eb828aa4a83120f77076f436938acb40e2e
M. Folkers <hello@twobrain.io>
2019-03-08 09:14:04 +0100

    Fixing css & js calls
    Missing echo function

--- snippets/htmlhead/css.php
+++ snippets/htmlhead/css.php
@@ -4,6 +4,6 @@ foreach (option('bnomei.htmlhead.css') as $dep) {
     if (class_exists('\Bnomei\Fingerprint')) {
         echo Bnomei\Fingerprint::css($dep, ['integrity'=>true]).PHP_EOL;
     } else {
-        css($dep);
+        echo css($dep);
     }
 }
--- snippets/htmlhead/js.php
+++ snippets/htmlhead/js.php
@@ -4,6 +4,6 @@ foreach (option('bnomei.htmlhead.js') as $dep) {
     if (class_exists('\Bnomei\Fingerprint')) {
         echo Bnomei\Fingerprint::js($dep, ['integrity'=>true]).PHP_EOL;
     } else {
-        js($dep);
+        echo js($dep);
     }
 }
