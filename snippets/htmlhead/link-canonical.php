5e483c6b99c8b2c1340eec5d2ec3a182d97e2175
Bruno Meilick <b@bnomei.com>
2022-03-16 08:56:56 +0000

    :art: fixed indention

--- snippets/htmlhead/link-canonical.php
+++ snippets/htmlhead/link-canonical.php
@@ -1,3 +1,3 @@
 <?php if (isset($urls)): foreach ($urls as $url): ?>
-    <link rel="canonical" href="<?= is_array($url) ? A::get($url, 'url') : $url ?>">
+<link rel="canonical" href="<?= is_array($url) ? A::get($url, 'url') : $url ?>">
 <?php endforeach; endif;
